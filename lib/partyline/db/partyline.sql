e0cd35004ad2c34706c606d107db272782a0f0b1
BrianOtto <brian.otto@zoho.com>
2023-08-19 00:23:01 -0700

    Added channel message logging

--- lib/partyline/db/partyline.sql
+++ lib/partyline/db/partyline.sql
@@ -1,11 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 CREATE DATABASE IF NOT EXISTS `partyline` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `partyline`;
 
@@ -23,6 +18,40 @@ CREATE TABLE `channel` (
   `tag3` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `id_ur` (`id_ur`),
+  ADD KEY `tag1` (`tag1`),
+  ADD KEY `tag2` (`tag2`),
+  ADD KEY `tag3` (`tag3`);
+
+ALTER TABLE `channel`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+DROP TABLE IF EXISTS `channel_message`;
+CREATE TABLE `channel_message` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_cl` int(10) UNSIGNED NOT NULL,
+  `id_ur` int(10) UNSIGNED NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `updated` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `updated_id_ur` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `deleted` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `deleted_id_ur` int(10) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `channel_message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cl` (`id_cl`),
+  ADD KEY `id_ur` (`id_ur`),
+  ADD KEY `updated_id_ur` (`updated_id_ur`),
+  ADD KEY `deleted_id_ur` (`deleted_id_ur`);
+
+ALTER TABLE `channel_message`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -34,24 +63,9 @@ CREATE TABLE `user` (
   `login_remember` tinyint(1) UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-ALTER TABLE `channel`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`),
-  ADD KEY `id_ur` (`id_ur`),
-  ADD KEY `tag1` (`tag1`),
-  ADD KEY `tag2` (`tag2`),
-  ADD KEY `tag3` (`tag3`);
-
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
-
-ALTER TABLE `channel`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 ALTER TABLE `user`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
